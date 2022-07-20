fe7853d884ffa5212940d93dc906109a4fb8446e
Solod Sergey <Solod098@gmail.com>
2022-07-20 15:06:54 +0300

    add slick-slider to gallery

--- js/slick.js
+++ js/slick.js
@@ -5,6 +5,43 @@ $(function () {
     slidesToScroll: 1,
     arrows: false,
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          speed: 1000,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          fade: true,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          speed: 1000,
+        },
+      },
+      // {
+      //   breakpoint: 480,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1,
+      //   },
+      // },
+    ],
   });
 });
 
