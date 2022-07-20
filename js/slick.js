8eb740633210f746d81e2bf48e97839f602f6857
Solod Sergey <Solod098@gmail.com>
2022-07-20 12:56:30 +0300

    main-question + main-ask-us = ok

---
+++ js/slick.js
@@ -0,0 +1,15 @@
+$(function () {
+  $(".gallery__list").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+  });
+});
+
+// $(document).ready(function () {
+//   $('.your-class').slick({
+//     setting-name: setting-value
+//   });
+// });
